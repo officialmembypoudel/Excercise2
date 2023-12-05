43e47983fc9ef5778ab8c6552c6971952bec1b1f
Memby Poudel <officialmembypoudel@gmail.com>
2023-12-05 21:15:08 +0545

    First Commit

---
+++ Excercise2/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Excercise2
+{
+	public class BankAccount
+	{
+		private string accountNumber;
+		private decimal balance = 0;
+		public BankAccount(string accountNumber)
+		{
+			this.accountNumber = accountNumber;
+		}
+
+		public void Deposit(decimal amount)
+		{
+			balance += amount;
+		}
+
+		public void Withdraw(decimal amount)
+		{
+			balance -= amount;
+		}
+
+		public void ShowBalance()
+		{
+			Console.WriteLine($"The remaining balance of {accountNumber} account is ${balance}.");
+		}
+	}
+}
+
---
+++ Excercise2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Excercise2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Running, Program!");
+
+        BankAccount ba = new BankAccount("s100");
+        ba.Deposit(1000);
+        ba.Withdraw(500);
+        ba.ShowBalance();
+
+        Console.ReadKey();
+    }
+}
+
